c22ba725422ff2f2b40f6b2607b0aa29e8d63811
Fatos Rama <fatosrama.fr@gmail.com>
2026-02-27 15:44:39 +0100

    Just removed the pool from index and imported it from db.

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,16 @@
 const express  = require('express');
 const cors = require('cors');
-const {Pool} = require('pg');
+const pool = require('./db');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'backendtest',
-    password:'fatos2005',
-    port: 5432,
-});
 
 app.post('/register', async (req, res) => {
   const { name, surname, email, phone_number, password } = req.body;
 
   try {
-    await pool.query(
+    const result = await pool.query(
       'INSERT INTO registered_user (name, surname, email, phone_number, password) VALUES ($1, $2, $3, $4, $5)',
       [name, surname, email, phone_number, password]
     );
@@ -28,6 +21,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Backend running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
